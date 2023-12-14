87f613b472733b6caa3528c45968bc60f1841059
rafaelcl292 <rafaelcl@al.insper.edu.br>
2023-12-14 13:55:34 -0300

    cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -49,9 +49,11 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ['<ESC>'] = cmp.mapping.close(),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
