6f7e4d3043ccf1bd966d127c2abd4012a3cebca6
rafaelcl292 <rafaelcl@al.insper.edu.br>
2024-03-03 11:02:02 -0300

    feat: enable for gitcommit
    also added the copilot_enabled for turning off by default when needed

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,3 @@
-vim.g.copilot_filetypes = { markdown = true }
+vim.g.copilot_enabled = true
+vim.g.copilot_filetypes = { markdown = true, gitcommit = true }
 vim.keymap.set('i', '<C-q>', '<Plug>(copilot-dismiss)')
