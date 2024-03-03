e97b2f7c4430014871a49c6bedae44e0604087ce
rafaelcl292 <rafaelcl@al.insper.edu.br>
2024-03-03 10:52:27 -0300

    fix: commit telescopico

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -14,22 +14,34 @@ telescope.setup({
 
 telescope.load_extension('fzf')
 local builtin = require('telescope.builtin')
+local map = vim.keymap.set
 
-vim.keymap.set({ 'n', 'v' }, '<leader>fa', builtin.find_files)
-vim.keymap.set({ 'n', 'v' }, '<leader>fr', builtin.git_files)
-vim.keymap.set({ 'n', 'v' }, '<leader>fh', builtin.help_tags)
-vim.keymap.set({ 'n', 'v' }, '<leader>fo', builtin.oldfiles)
-vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.live_grep)
-vim.keymap.set({ 'n', 'v' }, '<leader>fb', builtin.buffers)
-vim.keymap.set({ 'n', 'v' }, '<leader>fc', builtin.commands)
-vim.keymap.set({ 'n', 'v' }, '<leader>fd', function()
-    local find_command = { 'fdfind', '--type', 'd' }
+map({ 'n', 'v' }, '<leader>fa', builtin.find_files)
+map({ 'n', 'v' }, '<leader>fg', builtin.git_files)
+map({ 'n', 'v' }, '<leader>fh', builtin.help_tags)
+map({ 'n', 'v' }, '<leader>fo', builtin.oldfiles)
+map({ 'n', 'v' }, '<leader>fs', builtin.live_grep)
+map({ 'n', 'v' }, '<leader>fb', builtin.buffers)
+map({ 'n', 'v' }, '<leader>fc', builtin.commands)
+map({ 'n', 'v' }, '<leader>fk', builtin.keymaps)
+map({ 'n', 'v' }, '<leader>fD', builtin.diagnostics)
+map({ 'n', 'v' }, '<leader>fd', function()
     require('telescope.pickers')
         .new({}, {
             prompt_title = 'Find Directory',
-            finder = require('telescope.finders').new_oneshot_job(find_command),
             sorter = require('telescope.sorters').get_generic_fuzzy_sorter(),
+            finder = require('telescope.finders').new_oneshot_job(
+                { 'fdfind', '--type', 'd' },
+                {}
+            ),
         })
         :find()
-end, {})
-vim.keymap.set({ 'n', 'v' }, '<leader>fg', telescope.extensions.lazygit.lazygit)
+    vim.api.nvim_feedkeys('./', 'n', true)
+end, { desc = 'Find Directory' })
+map('n', '<leader>f/', function()
+    builtin.current_buffer_fuzzy_find(
+        require('telescope.themes').get_dropdown({
+            previewer = false,
+        })
+    )
+end, { desc = 'Search in current buffer' })
