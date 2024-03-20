24fc8a864965d0ac2e395cc028fded0ce2ee7c66
rafaelcl292 <rafaelcl@al.insper.edu.br>
2024-03-20 01:05:27 -0300

    feat: add filetypes for copilot

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,20 @@
 local lazygit_config = function()
-    vim.keymap.set({ 'n', 'v' }, '<leader>g', ':LazyGit<CR>', { silent = true })
+    local modes = { 'n', 'v' }
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set(modes, '<leader>g', ':LazyGit<CR>', opts)
     vim.g.lazygit_floating_window_scaling_factor = 1
 end
 
 local copilot_config = function()
     vim.g.copilot_enabled = true
-    vim.g.copilot_filetypes = { markdown = true, gitcommit = true }
+    vim.g.copilot_filetypes =
+        { markdown = true, gitcommit = true, text = true, [''] = true }
     vim.keymap.set('i', '<C-q>', '<Plug>(copilot-dismiss)')
 end
 
 return {
-    {'github/copilot.vim', config = copilot_config },
-    {'lewis6991/gitsigns.nvim', config = true },
+    { 'github/copilot.vim', config = copilot_config },
+    { 'lewis6991/gitsigns.nvim', config = true },
     {
         'kdheepak/lazygit.nvim',
         dependencies = {
