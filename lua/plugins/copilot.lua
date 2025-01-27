d8a6184b396a04c98abc9d07cc25cee8cead0b05
rafaelcl292 <rafaelcl@al.insper.edu.br>
2025-01-27 15:36:56 -0300

    fix: supertab

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,33 +1,54 @@
-return {
-    'zbirenbaum/copilot.lua',
-    event = 'InsertEnter',
-    config = {
+local M = { 'zbirenbaum/copilot.lua' }
+
+M.event = 'InsertEnter'
+
+local opts = {
+    enabled = true,
+    keymap = {
+        accept = false,
+        dismiss = false,
+        accept_word = false,
+        jump_prev = false,
+        jump_next = false,
+        refresh = false,
+        open = false,
+    },
+    filetypes = {
+        ['copilot-chat'] = false,
+        lua = true,
+        ['*'] = true,
+    },
+    suggestion = {
         enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
         keymap = {
             accept = false,
-            dismiss = false,
-            accept_word = false,
-            jump_prev = false,
-            jump_next = false,
-            refresh = false,
-            open = false,
-        },
-        filetypes = {
-            ['copilot-chat'] = false,
-            lua = true,
-            ['*'] = true,
-        },
-        suggestion = {
-            enabled = true,
-            auto_trigger = true,
-            hide_during_completion = false,
-            keymap = {
-                accept = '<Tab>',
-                dismiss = '<C-q>',
-                accept_word = '<C-a>',
-                next = false,
-                prev = false,
-            },
+            dismiss = '<C-q>',
+            accept_word = '<C-a>',
+            next = false,
+            prev = false,
         },
     },
 }
+
+M.config = function()
+    local copilot = require('copilot')
+    local suggestion = require('copilot.suggestion')
+    local api = vim.api
+
+    copilot.setup(opts)
+    local tab_key = api.nvim_replace_termcodes('<Tab>', true, false, true)
+
+    local function supertab()
+        if suggestion.is_visible() then
+            suggestion.accept()
+        else
+            api.nvim_feedkeys(tab_key, 'n', false)
+        end
+    end
+
+    vim.keymap.set('i', '<Tab>', supertab, { silent = true })
+end
+
+return M
